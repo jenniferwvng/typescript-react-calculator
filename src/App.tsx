43baf94cd94e3665cbd2b5a7e668a27822e4058a
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-08-19 14:34:10 +0200

    handleNumClick fixed logic error

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
     numAfterOperand: 0
   });
 
-
   const handleNumClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     let button = e.target as HTMLButtonElement;
 
@@ -20,25 +19,23 @@ function App() {
     //när klart, vid onclick på = ska funktion använda dessa property values och addera/subtrahera/dividera/multiplicera (calculate) beroende på vald operand 
     //därefter kan du välja att antingen AC (radera alla property values), eller att forsätta genom att ta calculatedSum som ny numBeforeOperand ifall du klickar = istället för AC
 
-
-    //if clickedoperand, numafteroperand, else if clickedoperand && calculatedSum !== 0, numAfterOperand === calculatedSum (skriv över) och invoke calculate vid press =, else numBeforeOperand (dvs )
     if (clickedOperand) {
       setCalculateStorage(prevState => ({
         ...prevState, 
         numAfterOperand: Number(button.value)
-      }))
-      console.log(calculateStorage.calculatedSum)
+      }))      
     } 
     
     if (clickedOperand && calculateStorage.calculatedSum !== 0) {
-      console.log('calculated sum is now not longer 0')
-      //If sum becomes 0 after subtraction this wont work anymore: could change initial state to null and use it as comparator?
+      //If sum becomes 0 while calculating this wont work anymore: could change initial state to null and use it as comparator? flag?
       setCalculateStorage(prevState => ({
         ...prevState, 
         numBeforeOperand: prevState.calculatedSum,
         calculatedSum: 0
       }))
-    } else {
+    } 
+
+    if (!clickedOperand) {
       setCalculateStorage(prevState => ({
         ...prevState, 
         numBeforeOperand: Number(button.value)
@@ -79,7 +76,7 @@ function App() {
     } else if (calculateStorage.operand === '-') {
       setCalculateStorage(prevState => ({
         ...prevState, 
-        calculatedSum: prevState.numBeforeOperand - prevState.numAfterOperand
+        calculatedSum: Number(prevState.numBeforeOperand - prevState.numAfterOperand)
       }));  
     } else if (calculateStorage.operand === '*') {
       setCalculateStorage(prevState => ({
@@ -94,46 +91,53 @@ function App() {
     }
   }
 
-  console.log('Calculated sum:' + calculateStorage.calculatedSum)
-
-  
-  console.log('operand:' + calculateStorage.operand)
-  console.log('numBeforeOperand:' + calculateStorage.numBeforeOperand)
-  console.log('numAfterOperand:' + calculateStorage.numAfterOperand)
-  console.log('calculatedSum:' + calculateStorage.calculatedSum)
-  
-
-
   return (
     <div className="App">
       <header>
         <h1>Typescript Calculator</h1>
-        <span>
-          <p>{calculateStorage.numBeforeOperand}</p>
-          <p>{calculateStorage.operand}</p>
-          <p>{calculateStorage.numAfterOperand}</p>
-          <p style={{color: 'red'}}>{calculateStorage.calculatedSum}</p>
-        </span>
-        <button name="operand" value={'+'} onClick={handleOperandClick}>+</button>
-        <button name="operand" value={'-'} onClick={handleOperandClick}>-</button>
-        <button name="operand" value={'/'} onClick={handleOperandClick}>/</button>
-        <button name="operand" value={'*'} onClick={handleOperandClick}>*</button>
-
-        <button name="calculate" value={'='} onClick={handleCalculation} style={{backgroundColor: 'red'}}>=</button>
-        <button name="reset" value={'AC'} onClick={handleReset} style={{backgroundColor: 'orange'}}>AC</button>
-        
-        <button value={1} onClick={handleNumClick}>1</button>
-        <button value={2} onClick={handleNumClick}>2</button>
-        <button value={3} onClick={handleNumClick}>3</button>
-        <button value={4} onClick={handleNumClick}>4</button>
-        <button value={5} onClick={handleNumClick}>5</button>
-        <button value={6} onClick={handleNumClick}>6</button>
-        <button value={7} onClick={handleNumClick}>7</button>
-        <button value={8} onClick={handleNumClick}>8</button>
-        <button value={9} onClick={handleNumClick}>9</button>
-        <button value={0} onClick={handleNumClick}>0</button>
-      
       </header>
+      <main>
+        <div>
+          <span>
+            <p>{calculateStorage.numBeforeOperand}</p>
+            <p>{calculateStorage.operand}</p>
+            <p>{calculateStorage.numAfterOperand}</p>
+            <p style={{color: 'red'}}>{calculateStorage.calculatedSum}</p>
+          </span>
+        </div>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <span style={{display: 'flex', flexDirection: 'column'}}>
+          <button name="reset" value={'AC'} onClick={handleReset} style={{backgroundColor: 'lightgrey', border: 'none', padding: '25px', borderRadius: '50%'}}>AC</button>
+          <button value={7} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>7</button>
+          <button value={4} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>4</button>
+          <button value={1} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>1</button>
+          <button value={0} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>0</button>
+        </span>
+
+        <span style={{display: 'flex', flexDirection: 'column'}}>
+          <button style={{backgroundColor: 'lightgrey', border: 'none', padding: '25px', borderRadius: '50%'}}></button>
+          <button value={8} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>8</button>
+          <button value={5} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>5</button>
+          <button value={2} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>2</button>
+        </span>
+
+        <span style={{display: 'flex', flexDirection: 'column'}}>
+          <button style={{backgroundColor: 'lightgrey', border: 'none', padding: '25px', borderRadius: '50%'}}></button>
+          <button value={9} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>9</button>
+          <button value={6} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>6</button>
+          <button value={3} onClick={handleNumClick} style={{backgroundColor: 'grey', border: 'none', padding: '25px', borderRadius: '50%'}}>3</button>
+        </span>
+
+        <span style={{display: 'flex', flexDirection: 'column'}}>
+          <button name="operand" value={'/'} onClick={handleOperandClick} style={{backgroundColor: 'orange', border: 'none', padding: '25px', borderRadius: '50%'}}>/</button>
+          <button name="operand" value={'*'} onClick={handleOperandClick} style={{backgroundColor: 'orange', border: 'none', padding: '25px', borderRadius: '50%'}}>*</button>
+          <button name="operand" value={'-'} onClick={handleOperandClick} style={{backgroundColor: 'orange', border: 'none', padding: '25px', borderRadius: '50%'}}>-</button>        
+          <button name="operand" value={'+'} onClick={handleOperandClick} style={{backgroundColor: 'orange', border: 'none', padding: '25px', borderRadius: '50%'}}>+</button>
+          <button name="calculate" value={'='} onClick={handleCalculation} style={{backgroundColor: 'orange', border: 'none', padding: '25px', borderRadius: '50%'}}>=</button>   
+        </span>      
+        </div>
+      </main>
     </div>
   );
 }
